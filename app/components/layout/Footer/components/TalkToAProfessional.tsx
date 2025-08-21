769302a859f685f95e836171146097d240b56be8
carloshenryck <ryck302@gmail.com>
2025-08-21 18:19:55 -0300

    feat: add responsiveness to talk to a professional component

--- app/components/layout/Footer/components/TalkToAProfessional.tsx
+++ app/components/layout/Footer/components/TalkToAProfessional.tsx
@@ -3,8 +3,13 @@ import Image from "next/image";
 
 export function TalkToAProfessional() {
   return (
-    <div className="w-full flex items-center justify-center text-black gap-12 py-[15svh]">
-      <p className="text-5xl xl:text-7xl font-bold">Fale com</p>
+    <div className="w-[90%] max-w-[1800px] flex max-[1350px]:flex-col items-center justify-center text-black gap-12 py-[15svh] max-[1350px]:pt-[8svh] mx-auto">
+      <p className="text-center text-4xl sm:text-5xl lg:text-7xl font-bold">
+        Fale com{" "}
+        <span className="min-[1350px]:hidden font-ivy font-medium">
+          um profissional
+        </span>{" "}
+      </p>
 
       <div className="relative">
         <Image
@@ -20,14 +25,16 @@ export function TalkToAProfessional() {
         </div>
       </div>
 
-      <p className="font-ivy text-5xl xl:text-7xl">um profissional</p>
+      <p className="font-ivy text-7xl hidden min-[1350px]:block">
+        um profissional
+      </p>
 
       <Image
         src={"/borderless-logo-white.svg"}
         width={671}
         height={798}
         alt="borderless logo"
-        className="absolute z-[-1] opacity-30 brightness-125 w-[50%]"
+        className="absolute z-[-1] opacity-30 brightness-125 w-auto h-[100%] object-cover"
       />
     </div>
   );
