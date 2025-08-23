12fdfb4bf9b85b762c10928a3dc5d2fbae346d4c
carloshenryck <ryck302@gmail.com>
2025-08-23 12:54:24 -0300

    feat: add modules overview component

--- app/(subpages)/bootcamp-web3/page.tsx
+++ app/(subpages)/bootcamp-web3/page.tsx
@@ -1,4 +1,5 @@
 import { BootcampHeroSection } from "@/app/components/sections/bootcamp-web3/BootcampHeroSection";
+import { ModulesOverview } from "@/app/components/sections/bootcamp-web3/ModulesOverview";
 import { WhoShouldJoin } from "@/app/components/sections/bootcamp-web3/WhoShouldJoin";
 import { WhyThisBootcamp } from "@/app/components/sections/bootcamp-web3/WhyThisBootcamp";
 
@@ -8,6 +9,8 @@ export default function Page() {
       <BootcampHeroSection />
       <WhyThisBootcamp />
       <WhoShouldJoin />
+      <ModulesOverview />
+      <section className="h-svh"></section>
     </div>
   );
 }
