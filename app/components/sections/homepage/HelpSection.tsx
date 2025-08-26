c2ce3fec146b764f7293b32b06564169c7a7f801
carloshenryck <ryck302@gmail.com>
2025-08-25 22:51:13 -0300

    refactor: add href to help section

--- app/components/sections/homepage/HelpSection.tsx
+++ app/components/sections/homepage/HelpSection.tsx
@@ -11,16 +11,30 @@ export function HelpSection() {
     <section style={{ minHeight: "fit-content" }}>
       <FullWidthCard title={t("title")} description={t("description")}>
         <div className="w-full grid sm:grid-cols-2 gap-2 max-w-4xl">
-          <ExternalLink className="w-full flex-row-reverse justify-end gap-4 pl-1" href={'/'}>
+          <ExternalLink
+            className="w-full flex-row-reverse justify-end gap-4 pl-1"
+            href={"/psp-mentorship"}
+          >
             {t("mentorship")}
-          </ExternalLink> 
-          <ExternalLink className="w-full flex-row-reverse justify-end gap-4 pl-1" href={'/'}>
+          </ExternalLink>
+          <ExternalLink
+            className="w-full flex-row-reverse justify-end gap-4 pl-1"
+            href={"/for-founders-and-investors"}
+          >
             {t("incubator")}
           </ExternalLink>
-          <ExternalLink className="w-full flex-row-reverse justify-end gap-4 pl-1" href={'/'}>
+          <ExternalLink
+            target="_blank"
+            className="w-full flex-row-reverse justify-end gap-4 pl-1"
+            href={"https://community.borderlesscoding.com/"}
+          >
             {t("platform")}
           </ExternalLink>
-          <ExternalLink className="w-full flex-row-reverse justify-end gap-4 pl-1" href={'/'}>
+          <ExternalLink
+            target="_blank"
+            className="w-full flex-row-reverse justify-end gap-4 pl-1"
+            href={"/psp-mentorship"}
+          >
             {t("community")}
           </ExternalLink>
         </div>
