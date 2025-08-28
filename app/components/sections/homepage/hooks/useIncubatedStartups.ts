9a98f50f23cf8c1b43cadc65844cfc21c2725425
carloshenryck <ryck302@gmail.com>
2025-08-27 22:47:44 -0300

    fix: type error

--- app/components/sections/homepage/hooks/useIncubatedStartups.ts
+++ app/components/sections/homepage/hooks/useIncubatedStartups.ts
@@ -3,10 +3,18 @@
 import { useRichText } from "@/app/hooks/useRichText";
 import { useMessages } from "next-intl";
 
+interface Startup {
+  src: string;
+  title: string;
+  description: string;
+  buttonText: string;
+  href: string;
+}
+
 export function useIncubatedStartups() {
   const { rich } = useRichText("HomePage.IncubatedStartups");
   const messages = useMessages();
-  const startups = messages.HomePage.IncubatedStartups.startups;
+  const startups = messages.HomePage.IncubatedStartups.startups as Startup[];
 
   return {
     title: rich("title"),
