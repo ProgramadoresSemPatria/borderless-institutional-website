2712948b93cd3b1c86927d5ee63fde4ed9cab1f8
carloshenryck <ryck302@gmail.com>
2025-09-01 13:00:21 -0300

    fix: scroll smoother stopping animations when page changes

--- app/components/misc/GSAPWrapper.tsx
+++ app/components/misc/GSAPWrapper.tsx
@@ -3,10 +3,21 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { ScrollSmoother, ScrollTrigger, SplitText } from "gsap/all";
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
 
 gsap.registerPlugin(useGSAP, SplitText, ScrollTrigger, ScrollSmoother);
 
 export function GSAPWrapper({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    const smoother = ScrollSmoother.get();
+    if (smoother) {
+      smoother.scrollTop(0);
+    }
+  }, [pathname]);
+
   useGSAP(() => {
     ScrollSmoother.create({
       smooth: 1,
