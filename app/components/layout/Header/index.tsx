f67737c5c2e58037741414e0dae145362d1c597b
carloshenryck <ryck302@gmail.com>
2025-09-05 11:12:23 -0300

    fix: header blinking on mobile

--- app/components/layout/Header/index.tsx
+++ app/components/layout/Header/index.tsx
@@ -7,5 +7,15 @@ import { MobileHeader } from "./components/MobileHeader";
 export function Header() {
   const isMobile = useMediaQuery("(max-width: 1024px)");
 
-  return <>{isMobile ? <MobileHeader /> : <DesktopHeader />}</>;
+  return (
+    <>
+      {isMobile ? (
+        <MobileHeader />
+      ) : (
+        <div className="hidden lg:block">
+          <DesktopHeader />
+        </div>
+      )}
+    </>
+  );
 }
