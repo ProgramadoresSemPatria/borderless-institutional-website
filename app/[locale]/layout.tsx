c5b246d85b2a126428b320ef273875d169712607
carloshenryck <ryck302@gmail.com>
2026-01-28 00:36:32 -0300

    feat: add utm script

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -12,6 +12,7 @@ import { GSAPWrapper } from "../components/misc/GSAPWrapper";
 import "../globals.css";
 import { Suspense } from "react";
 import { createMetadata } from "../lib/seo";
+import Script from "next/script";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -54,6 +55,78 @@ export default async function RootLayout({
       <body
         className={`${montserrat.className} ${ivyPresto.variable} antialiased`}
       >
+        <Script id="add_utm" strategy="lazyOnload">
+          {`(function () {
+            let parametros = ["utm_source"];
+            const url = new URL(window.location.href);
+            const params = new URLSearchParams(url.search);
+
+            for (const [key] of params) {
+                if (!parametros.includes(key)) {
+                    parametros.push(key);
+                }
+            }
+
+            const urlParamsCapt = new URLSearchParams(window.location.search);
+            const urlParamsCaptReferrer = new URLSearchParams(document.referrer.split('?')[1] || '');
+            let utms = {};
+
+            parametros.forEach(el => {
+                if (el === "utm_source") {
+                    utms[el] = urlParamsCapt.get(el) ?? (document.referrer ? (urlParamsCaptReferrer.get(el) ?? new URL(document.referrer).hostname) : "direto");
+                } else {
+                    utms[el] = urlParamsCapt.get(el) ?? (urlParamsCaptReferrer.get(el) ?? "");
+                }
+            });
+
+            let scks = Object.values(utms).filter(value => value !== "");
+
+            let currentSckValues = [];
+            if (urlParamsCapt.get('sck')) {
+                currentSckValues = urlParamsCapt.get('sck').split('|');
+            }
+            scks = scks.filter(value => !currentSckValues.includes(value));
+
+            const updateLinks = (el, elURL) => {
+                const elSearchParams = new URLSearchParams(elURL.search);
+                let modified = false;
+                for (let key in utms) {
+                    if (!elSearchParams.has(key)) {
+                        elSearchParams.append(key, utms[key]);
+                        modified = true;
+                    }
+                }
+                if (!elSearchParams.has('sck') && scks.length > 0) {
+                    elSearchParams.append('sck', scks.join('|'));
+                    modified = true;
+                }
+                if (modified) {
+                    return elURL.origin + elURL.pathname + "?" + elSearchParams.toString();
+                }
+                return el.href;
+            };
+
+            document.querySelectorAll('a').forEach(el => {
+                const elURL = new URL(el.href);
+                if (!elURL.hash) {
+                    el.href = updateLinks(el, elURL);
+                }
+            });
+
+            document.querySelectorAll('iframe').forEach(iframe => {
+                let actualSrc = iframe.hasAttribute('data-src') ? iframe.getAttribute('data-src') : iframe.src;
+                if (actualSrc) {
+                    const iframeURL = new URL(actualSrc);
+                    if (iframe.hasAttribute('data-src')) {
+                        iframe.setAttribute('data-src', updateLinks(iframe, iframeURL));
+                    } else {
+                        iframe.src = updateLinks(iframe, iframeURL);
+                    }
+                }
+            });
+
+            })();`}
+        </Script>
         <Suspense>
           <NextIntlClientProvider>
             <GSAPWrapper>
