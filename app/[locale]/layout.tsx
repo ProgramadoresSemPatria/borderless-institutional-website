060edf62ae4ed90489410aad124af7f67d148fb0
carloshenryck <ryck302@gmail.com>
2026-01-28 20:43:15 -0300

    feat: add clarity

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -127,6 +127,14 @@ export default async function RootLayout({
 
             })();`}
         </Script>
+
+        <Script strategy="beforeInteractive">
+          {`(function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "uxzd86pivh");`}
+        </Script>
         <Suspense>
           <NextIntlClientProvider>
             <GSAPWrapper>
